7f52c8c24a10c52b44608ca96a8df22efbe48fe8
Debraj Banerjee <sevenindians777@gmail.com>
2025-02-23 11:07:20 +0530

    Updated UI

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageFilter, ImageChops, ImageEnhance
 import numpy as np
-import sys
-import os
+import customtkinter as ctk
+from customtkinter import filedialog
 
-def apply_vignette(input_path, output_path):
+def apply_vignette(input_path):
     # Open the input image
     img = Image.open(input_path).convert("RGB")
     width, height = img.size
@@ -39,48 +39,61 @@ def apply_vignette(input_path, output_path):
     enhancer = ImageEnhance.Brightness(img_with_blurred_borders)
     img_with_blurred_borders = enhancer.enhance(1.5)
 
-    # Save the result
-    img_with_blurred_borders.save(output_path)
+    # return the image with the vignette effect
+    return img_with_blurred_borders
 
+def process_image(img, output_path):
+    # Create a new image with a 16:9 aspect ratio and black background
+    width, height = 1920, 1080
+    new_img = Image.new("RGBA", (width, height), "black")
 
-def process_image(input_path, output_path):
-    # Open an image file
-    with Image.open(input_path) as img:
-        # Create a new image with a 16:9 aspect ratio and black background
-        width, height = 1920, 1080
-        new_img = Image.new("RGBA", (width, height), "black")
+    # Resize the image
+    img = img.resize((360, 360), Image.LANCZOS)
 
-        # Resize the image
-        img = img.resize((360, 360), Image.LANCZOS)
+    # Rotate the image and paste it into the new image
+    for angle in [0, 90, 180, 270]:
+        rotated_img = img.rotate(angle)
+        if rotated_img.mode != 'RGBA':
+            rotated_img = rotated_img.convert('RGBA')
+        if angle == 90:
+            x_offset = 985-540
+            y_offset = 540-180
+        elif angle == 180:
+            x_offset = 985-180
+            y_offset = 540+180
+        elif angle == 270:
+            x_offset = 985+180
+            y_offset = 540-180
+        else:
+            x_offset = 985-180
+            y_offset = 0
+        new_img.paste(rotated_img, (x_offset, y_offset), rotated_img)
 
-        # Rotate the image and paste it into the new image
-        for angle in [0, 90, 180, 270]:
-            rotated_img = img.rotate(angle)
-            if rotated_img.mode != 'RGBA':
-                rotated_img = rotated_img.convert('RGBA')
-            if angle == 90:
-                x_offset = 985-540
-                y_offset = 540-180
-            elif angle == 180:
-                x_offset = 985-180
-                y_offset = 540+180
-            elif angle == 270:
-                x_offset = 985+180
-                y_offset = 540-180
-            else:
-                x_offset = 985-180
-                y_offset = 0
-            new_img.paste(rotated_img, (x_offset, y_offset), rotated_img)
+    # Convert the new image to RGB mode before saving
+    new_img = new_img.convert('RGB')
+    new_img.save(output_path)
 
-        # Convert the new image to RGB mode before saving
-        new_img = new_img.convert('RGB')
-        new_img.save(output_path)
+def select_image():
+    root = ctk.CTk()
+    root.withdraw()  # Hide the root window
+    file_path = filedialog.askopenfilename(
+        title="Select an Image File",
+        filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp;*.tiff")]
+    )
+    return file_path
+
+def save_image():
+    root = ctk.CTk()
+    root.withdraw()  # Hide the root window
+    file_path = filedialog.asksaveasfilename(
+        title="Save Image As",
+        defaultextension=".jpg",
+        filetypes=[("JPEG files", "*.jpg"), ("PNG files", "*.png"), ("BMP files", "*.bmp"), ("TIFF files", "*.tiff")]
+    )
+    return file_path
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <input_image_path>")
-    else:
-        input_path = sys.argv[1]
-        apply_vignette(input_path, 'test.jpg')
-        process_image('test.jpg', input_path)
-        os.remove('test.jpg')
+    input_path = select_image()
+    temp = apply_vignette(input_path)
+    output_path = save_image()
+    process_image(temp, output_path)
